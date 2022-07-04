3967ff8980684d1cbf676f1e108df99baa7571c2
Elena Spytsyna <lspoke.larn@gmail.com>
2022-07-04 15:46:30 +0300

    add project

---
+++ src/main/java/com/lspoke/app/controller/MyServlet.java
@@ -0,0 +1,24 @@
+package com.lspoke.app.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // resp.getOutputStream().write("Hello World".getBytes());
+        PrintWriter pw = resp.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> Hello world! </h1>");
+        pw.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
